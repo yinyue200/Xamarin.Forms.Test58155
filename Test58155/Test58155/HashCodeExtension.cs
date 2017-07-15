ab0c648d22531eb18477a251e37a5a5917e26866
yinyue200 <yinyue200@outlook.com>
2017-07-15 08:44:12 +0800

    添加项目文件。

---
+++ Test58155/Test58155/HashCodeExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test58155
+{
+    [ContentProperty("Text")]
+    class HashCodeExtension : IMarkupExtension<string>
+    {
+        public string Text { get; set; }
+        public string ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Text.GetHashCode().ToString();
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Text.GetHashCode().ToString();
+        }
+    }
+}
---
+++ Test58155/Test58155/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Test58155
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage.Navigation.PushAsync(new Page1());
+        }
+    }
+}
